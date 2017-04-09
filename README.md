50b5ca111f460335cee43e45c74f41c75c11e7e1
Yusuf Zeren <ysfzrn@gmail.com>
2017-04-10 02:52:44 +0300

    ghpage

--- README.md
+++
@@ -1,2 +0,0 @@
-# styled-animated
-React Animation Library was made with styled-components like Animated.css
--- src/components/index.js
+++
@@ -1,8 +0,0 @@
-import Animated from "./Animated";
-
-import FadeIn from "./fade/FadeIn";
-import FadeInDown from "./fade/FadeInDown";
-import FadeInLeft from "./fade/FadeInLeft";
-
-export { FadeIn, FadeInDown, FadeInLeft };
-export default Animated;
